0bc6841b5d7ab4900e1dc84b4a4d79d28f405b8e
Leo Zhou <leologist@gmail.com>
2019-03-10 16:52:29 -0400

    Added extended QAOA (where evolution is not necessarily generated by objective/problem Hamiltonian

--- MATLAB/QAOA/GeneralHamiltonians/ExtQAOAGrad.m
+++ MATLAB/QAOA/GeneralHamiltonians/ExtQAOAGrad.m
@@ -1,12 +1,13 @@
-function [F, F_grad, psi] = GenQAOAGrad(p, HamC, HamB, param, psi_in, EvolC, EvolB)
-%GenQAOAGrad performs QAOA circuit with general Hamiltonian, and computes
-%gradient
+function [F, F_grad, psi] = ExtQAOAGrad(p, HamObj, HamC, HamB, param, psi_in, EvolC, EvolB)
+%ExtQAOAGrad performs extended-QAOA circuit with general Hamiltonian,
+%and computes gradient
 %
-%   [F, F_grad] = GenQAOAGrad(p, HamC, HamB, param, psi_in, EvolC, EvolB)
+%   [F, F_grad] = ExtQAOAGrad(p, HamObj, HamC, HamB, param, psi_in, EvolC, EvolB)
 %       N = number of particles
 %       p = QAOA level/depth
-%       HamC = problem Hamiltonian (trying to minimize)
-%       HamB = mixing Hamiltonian
+%       HamObj = objective Hamiltonian (trying to minimize)
+%       HamC = Z-type evolution generator 
+%       HamB = X-type evolution generator
 %       param = 2*p parameter values, i.e. [gammas, betas]
 %       psi_in = initial wavefunction (default=all one vector)
 %
@@ -24,15 +25,15 @@ betas = param(p+1:end);
 F_grad = zeros(numel(param),1);
 
 
-if nargin < 5
+if nargin < 6
     psi_in = 1/sqrt(length(HamC))*ones(length(HamC),1);
 end
 
-if nargin < 6
+if nargin < 7
     EvolC = @(psi, gamma) expmv(-1i*gamma, HamC, psi);
 end
 
-if nargin < 7
+if nargin < 8
     EvolB = @(psi, beta) expmv(-1i*beta, HamB, psi);
 end
 
@@ -43,7 +44,7 @@ for ind = 1:p
     psi(:,ind+1) = EvolB(EvolC(psi(:,ind), gammas(ind)), betas(ind));
 end
 
-psi(:,p+2) = HamC*psi(:, p+1);
+psi(:,p+2) = HamObj*psi(:, p+1);
 
 for ind = 1:p
     psi(:, ind+p+2) = EvolC(EvolB(psi(:,ind+p+1), -betas(p+1-ind)), -gammas(p+1-ind));
