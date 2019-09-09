d8cac5f2afa179b8da146b6ec36a60fd17df451c
Leo Zhou <leologist@gmail.com>
2019-09-09 13:13:04 -0600

    Get rid of legacy linear+fourier option in ConvertMaxCutParam

--- MATLAB/QAOA/IsingProblem/ConvertMaxCutParam.m
+++ MATLAB/QAOA/IsingProblem/ConvertMaxCutParam.m
@@ -1,24 +1,17 @@
-function [paramC, paramB] = ConvertMaxCutParam(param, direction, real_p, p_trunc, flagLFT)
+function [paramC, paramB] = ConvertMaxCutParam(param, direction, real_p, p_trunc)
 %ConvertMaxCutParam(param, direction, real_p, p_trunc)
 %   Convert between fourier and real space parameters for MaxCut QAOA
+%   using DST-4/DCT-4
 %
 %   [gamma, beta] = ConvertMaxCutParam(param, 1, real_p, p_trunc)
 %   [u,v] = ConvertMaxCutParam(param, -1, real_p, p_trunc)
 %   
 %   direction = 1 if going from [u,v] to [gamma, beta]; -1 from [gamma, beta] 
 %   real_p = length of gamma, beta (optional)
-%   p_trunc = length of u, v (optional)
+%   p_trunc = length of u, v (optional, default = length(param)/2)
 %
-%   Also accepts ConvertMaxCutParam(param, direction, real_p, p_trunc, flagLFT)
-%   optional argument: flagLFT true if using linear+fourier parametrization
-%                              false if using DST-4/DCT-4
-
 
 param = param(:)';
-
-if nargin <= 4
-    flagLFT = false;
-end
 
 if nargin <= 3 || isempty(p_trunc)
     p_trunc = length(param)/2;
@@ -30,13 +23,8 @@ end
 
 
 [si, k_ind] = meshgrid(1:real_p, 0:(p_trunc-1));
-if flagLFT
-    MA = (si/real_p).*(k_ind==0) + sin(k_ind*pi.*si/real_p).*(k_ind > 0);
-    MB = (1-(si-1)/real_p).*(k_ind==0) + sin(k_ind*pi.*(si-1)/real_p).*(k_ind > 0);
-else
-    MA = sin((k_ind + 1/2)*pi.*(si-1/2)/real_p);
-    MB = cos((k_ind + 1/2)*pi.*(si-1/2)/real_p);
-end
+MA = sin((k_ind + 1/2)*pi.*(si-1/2)/real_p);
+MB = cos((k_ind + 1/2)*pi.*(si-1/2)/real_p);
  
 if direction == 1
     u = param(1:p_trunc);
