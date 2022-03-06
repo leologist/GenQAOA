1a3067343dd06b339f7c83ae2dfd924f37ddba81
Leo Zhou <leologist@gmail.com>
2022-03-05 17:26:05 -0800

    Adding experimental code for Quantum MaxCut

---
+++ MATLAB/experiments/QuantumMaxCut/QAOA_4QMC_X_ZZ_hZ.m
@@ -0,0 +1,126 @@
+
+TRY_RING = false;
+
+N = 10;
+
+if TRY_RING % Look at the N-vertex ring graph
+
+    Adj = circshift(eye(N),1);
+    [r,c] = find(Adj);
+    J = [r,c, -ones(size(r))];
+    Adj = Adj + Adj';
+    
+    aZ = 1+mod(1:N,2);
+
+else % Create a 3-regular graph
+    [wG, Adj] = randRegGraph(N,3);
+
+    HC = CreateHamC_MaxCut(N, wG);
+
+    Ixs = find(HC == max(HC));
+    Ix = Ixs(randi(length(Ix)));
+    aZ = 1+ flip(de2bi(Ix-1,N));
+    
+    J = [wG, -ones(size(wG,1))];
+end
+
+
+
+figure(2)
+plot(graph(Adj));
+
+hZ = (-1).^aZ;
+
+MaxCut = max(HC);
+
+fprintf('*** N=%d\n', N);
+fprintf('    MaxCut = %d, degen = %d\n', MaxCut, nnz(HC==MaxCut));
+fprintf('    Using hZ with cut value = %d\n', (sum(Adj(:)) - hZ*Adj*hZ')/4);
+
+
+%%
+
+[QAOAhelperfcn, HamObj, HamC, HamZ, HamB, EvolC, EvolZ, EvolB] = SetupQMCHams(N, J, hZ);
+
+
+[V, D] = eigs(HamObj);
+
+D = diag(D);
+
+I_GS = D <= D(1)+1e-10;
+QMC_degen = nnz(I_GS);
+
+fprintf('Quantum MaxCut value\n')
+fprintf('E0 = %0.6f (degen = %d)\nE1 = %0.6f\n', D(1),QMC_degen, D(QMC_degen+1));
+
+%%
+sx = sparse([0,1; 1,0]);
+sy = sparse([0,-1i; 1i, 0]);
+sz = sparse(diag([1,-1]));
+allX = kronrec(sx, N);
+allZ = kronrec(sz, N);
+
+%%
+p = 10;
+
+param0 = 2*rand(p,3)-1;
+
+myfun = @(param) QAOAhelperfcn(p, param);
+
+options = optimoptions('fminunc','GradObj','on','Hessian','off','Display','off',...
+    'TolX',1e-5,'TolFun',1e-5, 'Algorithm', 'quasi-newton','PlotFcns',{@optimplotfval, @optimplotstepsize},...
+    'MaxFunEvals', Inf, 'MaxIter', Inf);
+
+[x, fval] = fminunc(myfun, param0, options);
+
+[F1, Fg1, psiQAOA] = myfun(x);
+
+fprintf('QAOA fval = %0.6f @ p=%d\n', fval, p);
+fprintf('\t P_GS = %0.4e \n', sum(abs(psiQAOA'*V(:,I_GS)).^2));
+
+%%
+zs = {[0;1],[1;0]};
+
+psi0 = 1;
+for ind = 1:N
+    psi0 = kron(psi0, zs{aZ(ind)});
+end
+
+
+paulis = {sx, sy, sz};
+
+HamYX = sparse(0);
+
+for ind = 1:size(J,1)
+    Ia = J(ind,1);
+    Ib = J(ind,2);
+    sA = paulis{aZ(Ia)};
+    sB = paulis{aZ(Ib)};
+    HamYX = HamYX + Ham2LTerm(sA, sB, Ia, Ib, N);
+end
+
+
+%%
+yfun = @(t) AGM_ansatz(N, HamObj, HamYX, t, psi0, J, aZ, paulis);
+
+[theta, fval2] = fminunc(yfun, rand*pi, options);
+
+[F2, Fg2, psi_AGM] = AGM_ansatz(N, HamObj, HamYX, theta, psi0, J, aZ, paulis);
+
+fprintf('AGM ansatz fval = %0.6f @ theta = %0.4f\n', fval2, theta);
+fprintf('\t P_GS = %0.4e \n', sum(abs(psi_AGM'*V(:,I_GS)).^2));
+
+%%
+
+function [F, Fgrad, psi] = AGM_ansatz(N, HamObj, HamYX, theta, psi0, J, aZ, paulis)
+    psi = psi0;
+    for ind = 1:size(J,1)
+        Ia = J(ind,1);
+        Ib = J(ind,2);
+        sA = paulis{aZ(Ia)};
+        sB = paulis{aZ(Ib)};
+        psi = cos(theta) * psi - 1i*sin(theta)*Ham2LTerm(sA, sB, Ia, Ib, N)*psi;
+    end
+    F = real(psi'*HamObj*psi);
+    Fgrad = real(1i*psi'*(HamYX* HamObj -  HamObj*HamYX)*psi);
+end
---
+++ MATLAB/experiments/QuantumMaxCut/SetupQMCHams.m
@@ -0,0 +1,74 @@
+function [QAOAhelperfcn, HamObj, ...
+    HamC, HamZ, HamB, EvolC, EvolZ, EvolB] =  SetupQMCHams(N, J, h)
+%SetupQMCHams sets up the Hamiltonians and evolution functions for QAOA
+%           in one-dimension using ZZ, Z, and X-type driver to optimize the
+%           XXZ Hamiltonian
+%
+%   Arguments: 
+%       N = system size
+%       J = an |E|x3 array of edges and weights whos rows are [i, j, J_ij]
+%       h = a |V|x1 array specifying coefficient in the Z driver
+%                   so that HamZ = \sum_i h_i Z_i as driver,
+%            [] (empty) if using \sum_i Z_i as driver
+%
+%   Returns:
+%       QAOAhelperfcn = @(p, param) an evolution function based on
+%                       MultiQAOAGrad, returns <HamObj> and its gradient
+%                       for a given p and a set of parameters param
+%       HamObj = the XXZ objective Hamiltonian, which is
+%                \sum_{i=1}^{n-1} (XX + YY + Delta*ZZ)_{i,i+1}
+%       HamC = ZZ-type driver (potentially has long-range interaction)
+%       HamZ = Z-type driver (\sum_i h_i Z_i)
+%       HamB = X-type driver (\sum_i X_i)
+%       EvolC = @(psi, gamma) evolves a given psi by exp(-1i*gamma*HamC)
+%       EvolZ = @(psi, alpha) evolves a given psi by exp(-1i*alpha*HamZ)
+%       EvolB = @(psi, beta) evolves a given psi by exp(-1i*beta*HamB)
+%
+
+
+%% Set up Hamiltonian
+sx = sparse([0,1; 1,0]);
+sy = sparse([0,-1i; 1i, 0]);
+sz = sparse(diag([1,-1]));
+
+% objective Hamiltonian
+HamObj = sparse(0);
+
+for ind = 1:size(J, 1)
+    Ia = J(ind,1);
+    Ib = J(ind, 2);
+    HamObj = HamObj + J(ind,3)/2 * (speye(2^N) - Ham2LTerm(sx, sx, Ia, Ib, N) -...
+        Ham2LTerm(sy, sy, Ia, Ib, N) - Ham2LTerm(sz, sz, Ia, Ib, N));
+%     HamObj = HamObj + J(ind,3)/2 * (speye(2^N) - Ham2LTerm(sz, sz, Ia, Ib, N));
+end
+
+
+
+% ZZ driver
+
+HamC = sparse(0);
+
+for ind = 1:size(J,1)
+    Ia = J(ind,1);
+    Ib = J(ind,2);
+    HamC = HamC + J(ind, 3) * Ham2LTerm(sz, sz, Ia, Ib, N);
+end
+
+if nargin <= 2 || isempty(h)
+    HamZ = krondist(sz, N);
+else
+    HamZ = krondist(sz, N, h);
+end
+
+
+HamB = krondist(sx, N);
+HamZdiag = full(diag(HamZ));
+HamCdiag = full(diag(HamC));
+
+EvolB = @(psi, beta) EvolHamB(N, beta, psi, false);
+EvolC = @(psi, gamma) exp(-1i*gamma*HamCdiag).*psi;
+EvolZ = @(psi, alpha) exp(-1i*alpha*HamZdiag).*psi;
+
+psi0 = ones(2^N,1)/sqrt(2^N);
+
+QAOAhelperfcn = @(p, param) MultiQAOAGrad(p, HamObj, {HamC, HamZ, HamB}, param, psi0, {EvolC, EvolZ, EvolB});
